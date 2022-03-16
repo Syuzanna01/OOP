55ad566205286bfca0ef54a877cc68f21e4262d8
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-03-16 16:23:32 +0400

    .

---
+++ Lesson9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace _2_Inheritance
+{
+    class Program
+    {
+
+        class Car
+        {
+            public string Brand;
+            public string Color = "blue";
+            internal int aa;
+        }
+        class BMW : Car
+        {
+            protected int price4 = 80000;
+            protected int priceX6 = 120000;
+
+        }
+        class Series4 : BMW
+        {
+            protected string Name = "Series4";
+            public Series4()
+            {
+                Console.WriteLine(Name);
+                Console.WriteLine(price4);
+            }
+            public void Benzik()
+            {
+                Console.WriteLine("benzin");
+                Console.WriteLine(Color);
+            }
+        }
+        class SeriesX6 : BMW
+        {
+            protected string Name = "SeriesX6";
+            public SeriesX6()
+            {
+                Console.WriteLine(Name);
+                Console.WriteLine(priceX6);
+            }
+            public void Dizel()
+            {
+                Console.WriteLine("dizel");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Series4 series = new Series4();
+            series.Benzik();
+
+            Console.WriteLine();
+
+            SeriesX6 seriesX6 = new SeriesX6();
+            seriesX6.Dizel();
+        }
+    }
+}
