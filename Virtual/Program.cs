c8e607e50b4d5c9ff6706b7da218569d71032ab3
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-03-18 17:20:28 +0400

    Lesson10

--- Virtual/Program.cs
+++ Virtual/Program.cs
@@ -17,17 +17,32 @@ namespace Virtual
             {
                 Console.WriteLine("Child");
             }
+
+            //public void Method()
+            //{
+            //    Console.WriteLine("Child");
+            //}
         }
         static void Main(string[] args)
         {
             Object obj1 = new Child();
-            obj1.Method();
+            obj1.Method();//Child
 
             Object obj2 = new Object();
-            obj2.Method();
+            obj2.Method();//Object
 
             Child obj3 = new Child();
-            obj3.Method();
+            obj3.Method();//Child
+
+
+            //Object obj1 = new Child();
+            //obj1.Method();//Object
+
+            //Object obj2 = new Object();
+            //obj2.Method();//Object
+
+            //Child obj3 = new Child();
+            //obj3.Method();//Child
         }
     }
 }
